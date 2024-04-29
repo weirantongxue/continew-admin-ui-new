6e8bb9f8287ce260b1da59294904523e0d7bc0d1
“knowledge-base-zp” <748536206@qq.com>
2024-04-29 22:13:07 +0800

    项目迁移

---
+++ src/views/ai/model/type.ts
@@ -0,0 +1,13 @@
+export interface ModelResp {
+  id: string
+  name: string
+  modelType: string
+  coverUrl: string
+  url: string
+  apiKey: string
+  callBack: string
+  resType: string
+  introduction: string
+  sort: string
+  status: string
+}
---
+++ src/views/ai/modelScript/type.ts
@@ -0,0 +1,11 @@
+export interface ModelScriptResp {
+  id: string
+  name: string
+  modelId: string
+  prompt: string
+  coverUrl: string
+  component: string
+  description: string
+  sort: string
+  status: string
+}
